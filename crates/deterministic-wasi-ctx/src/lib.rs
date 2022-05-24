9ec766a288b4b4796679ce9c6154435397d22982
Jeffrey Charles <jeff.charles@shopify.com>
2022-05-24 12:43:18 -0400

    Fix typo
    Co-authored-by: Saúl Cabrera <saulecabrera@gmail.com>

--- crates/deterministic-wasi-ctx/src/lib.rs
+++ crates/deterministic-wasi-ctx/src/lib.rs
@@ -13,7 +13,7 @@ pub fn build_wasi_ctx(random_seed: u64) -> WasiCtx {
     // as deterministic but not reproducible.
     // See https://rust-random.github.io/book/guide-rngs.html#basic-pseudo-random-number-generators-prngs
     // and https://docs.rs/rand_pcg/latest/rand_pcg/struct.Mcg128Xsl64.html for further details.
-    // Using a u64 to seed the generator since we cannot guarantee that the input for the seed will be not contain low
+    // Using a u64 to seed the generator since we cannot guarantee that the input for the seed will not contain low
     // Hamming Weight numbers like 0 and 1 and we still want sufficiently random output
     let random = Box::new(Pcg64Mcg::seed_from_u64(random_seed));
 
