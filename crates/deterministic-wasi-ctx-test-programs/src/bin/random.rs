ca884e9bb77a7037c83ec2c2efd53f496a3f3914
Ernie Makris <ernie.makris@shopify.com>
2023-10-12 13:20:13 -0400

    clippy, slow-vector-initialization.

--- crates/deterministic-wasi-ctx-test-programs/src/bin/random.rs
+++ crates/deterministic-wasi-ctx-test-programs/src/bin/random.rs
@@ -2,8 +2,7 @@ fn main() {}
 
 #[export_name = "random"]
 pub extern "C" fn random() -> i32 {
-    let mut buf = Vec::new();
-    buf.resize(1, 0);
+    let mut buf = vec![0; 1];
     unsafe {
         wasi::random_get(buf.as_mut_ptr(), 1).expect("random_get failed");
     }
@@ -12,8 +11,7 @@ pub extern "C" fn random() -> i32 {
 
 #[export_name = "random_two_invocations"]
 pub extern "C" fn random_two_invocations() -> i32 {
-    let mut buf = Vec::new();
-    buf.resize(1, 0);
+    let mut buf = vec![0; 1];
     unsafe {
         wasi::random_get(buf.as_mut_ptr(), 1).expect("random_get failed");
         wasi::random_get(buf.as_mut_ptr(), 1).expect("random_get failed");
