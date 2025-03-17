2e0e64652b74f0cf2431dcf59d511f47d5abb179
Jeffrey Charles <jeff.charles@shopify.com>
2025-03-17 14:19:15 -0400

    Refactor integration tests (#81)

--- crates/deterministic-wasi-ctx/tests/random.rs
+++ crates/deterministic-wasi-ctx/tests/random.rs
@@ -1,17 +1,23 @@
+use anyhow::Result;
+
 mod common;
 
 #[test]
-fn test_random() {
-    let (store, instance) = common::create_instance("random.wasm");
-    let result = common::invoke_func::<(), i32>(store, instance, "random", ());
-    let expected_random_output = 155;
-    assert_eq!(result, expected_random_output);
+fn test_random() -> Result<()> {
+    common::test_instance("random.wasm", |invoke_func| {
+        let result: i32 = invoke_func("random", ())?;
+        let expected_random_output = 155;
+        assert_eq!(result, expected_random_output);
+        Ok(())
+    })
 }
 
 #[test]
-fn test_random_two_invocations() {
-    let (store, instance) = common::create_instance("random.wasm");
-    let result = common::invoke_func::<(), i32>(store, instance, "random_two_invocations", ());
-    let expected_random_output = 111;
-    assert_eq!(result, expected_random_output);
+fn test_random_two_invocations() -> Result<()> {
+    common::test_instance("random.wasm", |invoke_func| {
+        let result: i32 = invoke_func("random_two_invocations", ())?;
+        let expected_random_output = 111;
+        assert_eq!(result, expected_random_output);
+        Ok(())
+    })
 }
