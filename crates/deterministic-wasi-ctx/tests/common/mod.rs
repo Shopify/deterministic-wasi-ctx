537ccc9fa4cb426eae2996c81bbef188ce5c0e90
Jeff Charles <jeff.charles@shopify.com>
2025-03-17 11:53:55 -0400

    Refactor integration tests

--- crates/deterministic-wasi-ctx/tests/common/mod.rs
+++ crates/deterministic-wasi-ctx/tests/common/mod.rs
@@ -1,26 +1,34 @@
+use anyhow::Result;
 use std::path::Path;
 use wasi_common::WasiCtx;
 use wasmtime::*;
 
-pub fn create_instance(module_name: &str) -> (Store<WasiCtx>, Instance) {
+pub fn test_instance<Params, Results, F>(module_name: &str, testcase: F) -> Result<()>
+where
+    Params: WasmParams,
+    Results: WasmResults,
+    F: Fn(Box<dyn FnOnce(&str, Params) -> Result<Results>>) -> Result<()>,
+{
     let wasi = deterministic_wasi_ctx::build_wasi_ctx();
     let engine = Engine::default();
     let mut linker = Linker::new(&engine);
-    wasi_common::sync::add_to_linker(&mut linker, |s| s).unwrap();
+    wasi_common::sync::add_to_linker(&mut linker, |s| s)?;
     let module_path = Path::new("../../target/wasm32-wasip1/debug").join(module_name);
-    let module = Module::from_file(&engine, module_path).unwrap();
+    let module = Module::from_file(&engine, module_path)?;
     let mut store = Store::new(&engine, wasi);
-    linker.module(&mut store, "", &module).unwrap();
-    let instance = linker.instantiate(&mut store, &module).unwrap();
-    (store, instance)
+    linker.module(&mut store, "", &module)?;
+    let instance = linker.instantiate(&mut store, &module)?;
+    testcase(Box::new(move |func_name, params| {
+        invoke_func(store, instance, func_name, params)
+    }))
 }
 
-pub fn invoke_func<Params, Results>(
+fn invoke_func<Params, Results>(
     mut store: Store<WasiCtx>,
     instance: Instance,
     func_name: &str,
     params: Params,
-) -> Results
+) -> Result<Results>
 where
     Params: WasmParams,
     Results: WasmResults,
@@ -28,6 +36,6 @@ where
     let answer = instance
         .get_func(&mut store, func_name)
         .unwrap_or_else(|| panic!("`{}` was not an exported function", func_name));
-    let answer = answer.typed::<Params, Results>(&store).unwrap();
-    answer.call(&mut store, params).unwrap()
+    let answer = answer.typed::<Params, Results>(&store)?;
+    answer.call(&mut store, params)
 }
--- crates/deterministic-wasi-ctx/tests/scheduler.rs
+++ crates/deterministic-wasi-ctx/tests/scheduler.rs
@@ -1,3 +1,4 @@
+use anyhow::Result;
 use std::time::Instant;
 
 mod common;
@@ -6,19 +7,21 @@ mod common;
 extern crate more_asserts;
 
 #[test]
-fn test_sleep() {
-    let (store, instance) = common::create_instance("scheduler.wasm");
-    let start = Instant::now();
-    common::invoke_func::<(), ()>(store, instance, "sleep", ());
-    let duration = start.elapsed();
-    assert_lt!(duration.as_millis(), 100);
+fn test_sleep() -> Result<()> {
+    common::test_instance::<_, (), _>("scheduler.wasm", |invoke_func| {
+        let start = Instant::now();
+        invoke_func("sleep", ())?;
+        let duration = start.elapsed();
+        assert_lt!(duration.as_millis(), 100);
+        Ok(())
+    })
 }
 
 #[test]
-fn test_yield() {
-    let (store, instance) = common::create_instance("scheduler.wasm");
-
-    // it's difficult to test that yielding isn't yielding in practice since sched_yield is very fast
-    // it's still worth testing that it doesn't panic
-    common::invoke_func::<(), ()>(store, instance, "yield", ());
+fn test_yield() -> Result<()> {
+    common::test_instance("scheduler.wasm", |invoke_func| {
+        // it's difficult to test that yielding isn't yielding in practice since sched_yield is very fast
+        // it's still worth testing that it doesn't panic
+        invoke_func("yield", ())
+    })
 }
